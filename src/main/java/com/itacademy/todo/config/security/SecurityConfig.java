f7d555e04b567a13686bc9e8edca607dcc057541
Andre Gvazdauskaite <andre.gvazdauskaite@gmail.com>
2022-03-28 21:02:12 +0300

    Added CORS filter

--- src/main/java/com/itacademy/todo/config/security/SecurityConfig.java
+++ src/main/java/com/itacademy/todo/config/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .and()
                 .httpBasic()
                 .authenticationEntryPoint(basicAuthenticationEntryPoint);
+        http.cors();
         return http.build();
     }
 
